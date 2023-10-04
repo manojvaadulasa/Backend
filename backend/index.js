87991a527ce43a2fd9f6cade0b74f12e919081c1
Manoj.V <venkimanoj2@gmail.com>
2023-10-04 08:41:15 +0530

    [Attempt #9]

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,29 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 const environment = process.env.ENVIRONMENT || 'development';
-const FormMain = require('./Routes/FormMain')
+const FormMain = require('./Routes/FormMain');
 const connectDB = require('./Middleware/mongoose');
 
 connectDB();
 
-app.use(cors());
-app.use(express.json());
+app.use(cors(
+  {
+    origin: ["https://manojvfullstackexample.in/*"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true
+  }
+));
+  app.use(express.json());
 
+
+
+app.get("/", (req, res) => {
+  res.json("Hello");
+})
 app.use('/api/details', FormMain);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port} in ${environment} mode`);
-});
+  console.log("Server is Running")
+})
